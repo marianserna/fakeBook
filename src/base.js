ff79da273e109af5488dafb453a7f8459129f659
Marian Serna <marianhalliday@gmail.com>
2017-05-13 11:15:43 -0400

    Configured Firebase + Added re-base + Configured Facebook Auth

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyAvnbLY43aErLmF5rpOpqrEG7OfKmJDWMg",
+  authDomain: "fakebook-408c8.firebaseapp.com",
+  databaseURL: "https://fakebook-408c8.firebaseio.com"
+});
+
+export default base;
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,16 +1,64 @@
 import React, { Component } from 'react';
-import Form from './Form';
-import Profile from './Profile';
-import Grid from './Grid';
+import base from '../base';
 
-import { Link } from 'react-router-dom';
-import { TweenMax } from 'gsap';
+// import { Link } from 'react-router-dom';
+// import { TweenMax } from 'gsap';
 
-export default class App extends Component {
+export default class Landing extends Component {
+  constructor() {
+    super();
+
+    this.provider = new base.auth.FacebookAuthProvider();
+  }
+
+  componentWillMount() {
+    base.auth().getRedirectResult().then((result) => {
+      if (result.credential) {
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        const token = result.credential.accessToken;
+        // ...
+      }
+      // The signed-in user info.
+      let user = result.user;
+      this.saveUser(user);
+    }).catch(function(error) {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      const credential = error.credential;
+      // ...
+    });
+  }
+
+  redirect = (e) => {
+    e.preventDefault();
+    base.auth().signInWithRedirect(this.provider);
+  }
+
+  saveUser = (user) => {
+    console.log(user);
+    const data = {
+      name: user.displayName,
+      email: user.email,
+      photo: user.photoURL
+    }
+
+    base.post(`users/${user.uid}`, {
+      data: data
+    }).then(() => {
+      this.props.history.push('/form');
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        Welcome to React!
+        <a className="join" href="#" onClick={(e) => this.redirect(e)}>
+          JOIN THE CLUB
+        </a>
       </div>
     );
   }
