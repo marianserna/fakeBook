d17603b35bfe83ff46e2e59617234bd802c7136d
Marian Serna <marianhalliday@gmail.com>
2017-05-14 20:45:59 -0400

    Adding functionality to change images and filters

--- src/components/Form.js
+++ src/components/Form.js
@@ -15,7 +15,9 @@ export default class Form extends React.Component {
     this.state = {
       loading: true,
       user: null,
-      title: 'Developer'
+      title: 'Developer',
+      chosenFilter: 'una',
+      newImage: null
     }
   }
 
@@ -38,6 +40,25 @@ export default class Form extends React.Component {
     });
   }
 
+  changeFilter = (e, filter) => {
+    this.setState({
+      chosenFilter: filter
+    })
+  }
+
+  changeImage = (e) => {
+    // Read contents of file (img) stored in user computer using file or blob obj
+    const reader = new FileReader();
+    // Access the files associated to the input: Tell the reader to go read the file
+    reader.readAsDataURL(e.target.files[0]);
+
+    reader.addEventListener('load', () => {
+      this.setState({
+        newImage: reader.result
+      })
+    });
+  }
+
   render() {
     if (this.state.loading) {
       return(
@@ -53,15 +74,15 @@ export default class Form extends React.Component {
           <form>
             <div className="input-row">
               <span className="input-wrapper">
-                <input className="input-field" type="text" id="name" name="name" defaultValue={this.state.user.name} required ref={(input) => this.name = input}/>
                 <label className="input-label" htmlFor="name">Name</label>
+                <input className="input-field" type="text" id="name" name="name" defaultValue={this.state.user.name} required ref={(input) => this.name = input}/>
               </span>
             </div>
 
             <div className="input-row">
               <span className="input-wrapper">
-                <input className="input-field" type="text" id="description" name="description" required ref={(input) => this.description = input}/>
                 <label className="input-label" htmlFor="description">Description</label>
+                <input className="input-field" type="text" id="description" name="description" required ref={(input) => this.description = input}/>
               </span>
             </div>
 
@@ -72,21 +93,40 @@ export default class Form extends React.Component {
 
             <div className="input-row">
               <span className="input-wrapper">
-                <input className="input-field" type="url" id="linkedin" name="linkedin" required ref={(input) => this.linkedin = input}/>
                 <label className="input-label" htmlFor="linkedin">LinkedIn</label>
+                <input className="input-field" type="url" id="linkedin" name="linkedin" required ref={(input) => this.linkedin = input}/>
               </span>
             </div>
 
             <div className="profile-img">
-              <p>Choose a Filter</p>
-              <img src={this.state.user.photo} alt="profile image"/>
+              <img src={this.state.newImage || this.state.user.photo} alt="profile image"/>
+            </div>
+
+            <div className="input-row">
+              <span className="input-wrapper">
+                <input className="input-field" type="file" id="change-img" name="change-img" required ref={(input) => this.newImage = input} onChange={(e) => this.changeImage(e)} />
+                <label className="input-label" htmlFor="change-img">Change Image</label>
+              </span>
             </div>
 
             <div className="filters">
-              <CircleFilter photo={this.state.user.photo} />
-              <TextFilter photo={this.state.user.photo} text={'Hello!'} />
-              <UnaFilter photo={this.state.user.photo} />
-              <DesandroFilter photo={this.state.user.photo} />
+              <p>Choose a Filter</p>
+
+              <div className={`filter ${this.state.chosenFilter === 'circle' ? 'chosen' : ''}`} onClick={(e) => this.changeFilter(e, 'circle')}>
+                <CircleFilter photo={this.state.newImage || this.state.user.photo} />
+              </div>
+
+              <div className={`filter ${this.state.chosenFilter === 'text' ? 'chosen' : ''}`} onClick={(e) => this.changeFilter(e, 'text')}>
+                <TextFilter photo={this.state.newImage || this.state.user.photo} text={'Hello!'} />
+              </div>
+
+              <div className={`filter ${this.state.chosenFilter === 'una' ? 'chosen' : ''}`} onClick={(e) => this.changeFilter(e, 'una')}>
+                <UnaFilter photo={this.state.newImage || this.state.user.photo} />
+              </div>
+
+              <div className={`filter ${this.state.chosenFilter === 'desandro' ? 'chosen' : ''}`} onClick={(e) => this.changeFilter(e, 'desandro')}>
+                <DesandroFilter photo={this.state.newImage || this.state.user.photo} />
+              </div>
             </div>
 
             <button type="submit">Post Profile</button>
